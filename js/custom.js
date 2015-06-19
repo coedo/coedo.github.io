e2715a8bbee95ecac1bedfe70239bbc52365bd52
mon_sat <tnk@daijiten.info>
2015-06-19 16:00:49 +0900

    Update Facebook Like Box

--- js/custom.js
+++ js/custom.js
@@ -48,24 +48,6 @@ $('.flex-image').flexslider({
   animationSpeed: 3000    
 });
 
-/* Twitter */
-
-jQuery(function($){
-   $(".tweet").tweet({
-      username: "CoEdoKYB",
-      join_text: "auto",
-      avatar_size: 0,
-      count: 3,
-      auto_join_text_default: "we said,",
-      auto_join_text_ed: "we",
-      auto_join_text_ing: "we were",
-      auto_join_text_reply: "we replied to",
-      auto_join_text_url: "we were checking out",
-      loading_text: "loading tweets...",
-      template: "{text}"
-   });
-}); 
-
 /* Coming soon page twitter */
 
 jQuery(function($){
